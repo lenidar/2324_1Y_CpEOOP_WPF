8a404074eeeb7600f7574d8b87f15bd5b11843c3
lenidar <55443074+lenidar@users.noreply.github.com>
2024-06-03 14:29:02 +0800

    Add project files.

---
+++ 2324_1Y_CpEOOP_WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2324_1Y_CpEOOP_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int textLength = 100;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSample_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbSample.Text.Length > 0)
+            {
+                MessageBox.Show($"{txtbSample.Text} loves Krizan", "Krizan"
+                    , MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                lblSamples.Content += txtbSample.Text;
+                txtbSample.Text = "";
+            }
+            else
+                MessageBox.Show($"Jet loves Krizan", "Krizan"
+                    , MessageBoxButton.YesNo, MessageBoxImage.Warning);
+        }
+
+        private void txtbSample_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string temp = txtbSample.Text;
+            if(temp.Length > textLength)
+            {
+                temp = temp.Substring(0, textLength);
+                txtbSample.Text = temp;
+            }
+        }
+
+        private void btnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+            WindowManager.w1_status(true);
+        }
+    }
+}
---
+++ 2324_1Y_CpEOOP_WPF/WindowManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2324_1Y_CpEOOP_WPF
+{
+    internal static class WindowManager
+    {
+        public static Window1 w1 = null;
+        public static bool w1_exist = false;
+
+        public static void w1_status(bool open)
+        {
+            if(!w1_exist && open)
+            {
+                w1 = new Window1();
+                w1.Show();
+                w1_exist = true;
+            }
+            else if(!open)
+            {
+                w1_exist = false;
+            }
+        }
+    }
+}
